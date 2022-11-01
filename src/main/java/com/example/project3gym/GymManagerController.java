85c2f1bfcd657a0ce459c7b3d9588b908e9b5f6f
JohnFGC <jfgc2002@gmail.com>
2022-11-01 16:17:53 -0400

    kill me

--- src/main/java/com/example/project3gym/GymManagerController.java
+++ src/main/java/com/example/project3gym/GymManagerController.java
@@ -6,15 +6,14 @@ import javafx.scene.control.Label;
 
 public class GymManagerController {
     private MemberDatabase db = new MemberDatabase();
-    private fitnessmanager.ClassSchedule cs = new fitnessmanager.ClassSchedule();
+    private ClassSchedule cs = new ClassSchedule();
     @FXML
     private Label tempText;
 
     @FXML
     protected void onAddButtonClick() {
         tempText.setText("Enter in the following to register: ");
-        //add();
-
+        add();
     }
 
     @FXML
@@ -32,76 +31,74 @@ public class GymManagerController {
 
     }
 
-    private void add(String fname, String lname, String dob, String location, fitnessmanager.MembershipType membershipType){
-        if (canBeAdded(dob, location)){
-            fitnessmanager.Date expire = getExpirationDate(membershipType);
-            fitnessmanager.Member memberToAdd;
-            switch (membershipType){
-                case STANDARD:
-                    memberToAdd = new fitnessmanager.Member(fname, lname, dob, expire, location);
-                    break;
-                case FAMILY:
-                    memberToAdd = new fitnessmanager.Family(fname, lname, dob, expire, location);
-                    break;
-                case PREMIUM:
-                    memberToAdd = new fitnessmanager.Premium(fname, lname, dob, expire, location);
-                    break;
-                default:
-                    memberToAdd = null;
-            }
-            if (this.db.add(memberToAdd)){               //Checks if member has already been added to database
-                System.out.println(fname + " " + lname + " added.");
-            }
-            else {
-                System.out.println(fname + " " + lname + " is already in the database.");
-            }
+    private String add(String fname, String lname, String dob, String location, MembershipType membershipType){
+        String errorMessage = canBeAdded(dob, location);
+        if (errorMessage != null){
+            return errorMessage;
+        }
+        Date expire = getExpirationDate(membershipType);
+        Member memberToAdd;
+        switch (membershipType){
+            case STANDARD:
+                memberToAdd = new Member(fname, lname, dob, expire, location);
+                break;
+            case FAMILY:
+                memberToAdd = new Family(fname, lname, dob, expire, location);
+                break;
+            case PREMIUM:
+                memberToAdd = new Premium(fname, lname, dob, expire, location);
+                break;
+            default:
+                memberToAdd = null;
+        }
+        if (this.db.add(memberToAdd)){               //Checks if member has already been added to database
+            return (fname + " " + lname + " added.");
+        }
+        else {
+            return (fname + " " + lname + " is already in the database.");
         }
     }
 
-    private boolean canBeAdded(String dob, String location){
-        fitnessmanager.Date birthday = new fitnessmanager.Date(dob);
+    private String canBeAdded(String dob, String location){
+        Date birthday = new Date(dob);
         if (!birthday.isValid()){                                                       //Checks if birthday date is a valid date
-            System.out.println("DOB " + dob + ": invalid calendar date!");
-            return false;
+            return ("DOB " + dob + ": invalid calendar date!");
         }
-        fitnessmanager.Date today = new fitnessmanager.Date();
+        Date today = new Date();
         if (birthday.compareTo(today) >= 0){                                            //Checks if birthday date is before today
-            System.out.println("DOB " + dob + ": cannot be today or a future date!");
-            return false;
+            return ("DOB " + dob + ": cannot be today or a future date!");
         }
         if (!isOverEighteen(birthday, today)){                                          //Checks if member is over 18
-            System.out.println("DOB " + dob + ": must be 18 or older to join!");
-            return false;
+            return ("DOB " + dob + ": must be 18 or older to join!");
         }
-        if (fitnessmanager.Location.stringToLocation(location) == null){                                   //Checks if member's gym location exists
-            System.out.println(location + ": invalid location!");
-            return false;
+        if (Location.stringToLocation(location) == null){                                   //Checks if member's gym location exists
+            return (location + ": invalid location!");
         }
-        return true;
+        return null;
     }
 
-    private fitnessmanager.Date getExpirationDate(fitnessmanager.MembershipType membershipType){
-        fitnessmanager.Date today = new fitnessmanager.Date();
+    private Date getExpirationDate(MembershipType membershipType){
+        Date today = new Date();
         switch (membershipType){
             case STANDARD:
             case FAMILY:
                 int futureMonth = 3 + today.getMonth();
                 if (futureMonth > 12){
                     futureMonth = futureMonth - 12;
-                    return new fitnessmanager.Date(futureMonth, today.getDay(), today.getYear() + 1);
+                    return new Date(futureMonth, today.getDay(), today.getYear() + 1);
                 }
-                return new fitnessmanager.Date(futureMonth, today.getDay(), today.getYear());
+                return new Date(futureMonth, today.getDay(), today.getYear());
             case PREMIUM:
-                return new fitnessmanager.Date(today.getMonth(), today.getDay(), today.getYear() + 1);
+                return new Date(today.getMonth(), today.getDay(), today.getYear() + 1);
         }
         return null;
     }
 
-    private boolean isOverEighteen(fitnessmanager.Date birthday, fitnessmanager.Date today){
-        if (today.getYear() - birthday.getYear() > fitnessmanager.Constant.MINIMUM_AGE.getValue()){
+    private boolean isOverEighteen(Date birthday, Date today){
+        if (today.getYear() - birthday.getYear() > Constant.MINIMUM_AGE.getValue()){
             return true;
         }
-        if (today.getYear() - birthday.getYear() < fitnessmanager.Constant.MINIMUM_AGE.getValue()){
+        if (today.getYear() - birthday.getYear() < Constant.MINIMUM_AGE.getValue()){
             return false;
         }
         if (today.getMonth() > birthday.getMonth()){
